4d6417cae6394105d87efe9dc71cfc7c04184777
VillyFiki <maksstelmakh24@gmail.com>
2020-11-06 12:55:06 +0300

    Добавьте файлы проекта.

---
+++ ITGFinal/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITGFinal.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ITGFinal/Models/AuthModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ITGFinal.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
